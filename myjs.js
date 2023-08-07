50f78bfad80ac78738bfdcbac6739842de022153
Ayaaa99 <chz011@ucsd.edu>
2023-08-06 22:45:29 -0700

    sdf

--- myjs.js
+++ myjs.js
@@ -9,9 +9,25 @@ class projectCard extends HTMLElement {
         let projname = document.createElement('h2');
         let projimg = document.createElement('img');
         projimg.setAttribute("alt", "This is a image of the project.");
+
         let descript = document.createElement('p');
         let readmore = document.createElement('a');
         wrapper.append(projname,projimg,descript,readmore);
 
+        const projstyle = document.createElement('style');
+        projstyle.textContent = `.wrapper{
+            background-color: blue;
+        }
+        img{
+            width: 100%;
+        }`;
+
+        this.shadowRoot.append(projstyle,wrapper);
     }
+}
+
+customElements.define('project-card', projectCard);
+
+function init() {
+    
 }
